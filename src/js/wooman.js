f441895ee4b344563157567372afbc3bd14bfd61
Jonathan C <johnlejardinnier@gmail.com>
2018-02-15 15:47:37 +0100

    feat(app): add shop filters to get man, wooman, accessories and equipment

---
+++ src/js/wooman.js
@@ -0,0 +1,21 @@
+export default {
+    data() {
+        return {
+            products:[],
+            loading:true,
+        }
+    },
+    mounted() {
+        this.products = this.$resource('products/wooman', {}, {}, {
+            before: () => {
+                this.loading = true
+            },
+            after: () => this.loading = false
+        })
+        this.products.query().then((response) => {
+            this.products = response.data
+        }, (response) => {
+            console.log('error', response)
+        })
+    }
+}
---
+++ src/pages/Man.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="ui container">
+        <h1 class="ui header">Collection homme</h1>
+        <!--<div class="ui fluid grid centered container">
+            <div class="ui active loader" v-if="loading"></div>
+            <div class="row">
+                <div class="link ui cards">
+                    <router-link :to="{ name: 'shop.product.view', params:{id:user.id}}" class="ui medium card" v-for="user in users" :key="user.id">
+                        <div class="content">
+                            <p>{{user.id}}</p>
+                            <p>{{user.name}}</p>
+                            <p>{{user.email}}</p>
+                        </div>
+                    </router-link>
+                </div>
+            </div>
+        </div>-->
+        <div class="row">
+            <div class="ui active loader" v-if="loading"></div>
+            <div class="ui four wide column doubling  link cards">
+                <router-link :to="{ name: 'shop.product.view', params:{id:product.product_id}}" class="ui card"  v-for="product in products" :key="product.product_id">
+                    <div class="ui small centered image">
+                        <img :src="product.featured.url">
+                    </div>
+                    <div class="content">
+                        <div class="meta">
+                            <span class="ui small tag label category">{{product.type.name}}</span>
+                        </div>
+                        <div class="ui clearing divider"></div>
+                        <div class="description">
+                            <div class="ui small left aligned header">{{product.name}}</div>
+                        </div>
+                    </div>
+                    <div class="extra content">
+                        <p class="center aligned"><strong class="ui header">{{product.price}}€</strong></p>
+                    </div>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+<script src="../js/man.js"></script>
