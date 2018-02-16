6906febe626a1de440ef7ca25301402367564a61
Babouze <ivorydu13@gmail.com>
2018-02-16 20:25:48 +0100

    feat(app): menu mobile top width corrected

--- src/components/menuMobileTop.vue
+++ src/components/menuMobileTop.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="ui mobile only grid">
         <div class="ui top fixed menu">
-            <div class="ui grid">
+            <div class="ui grid" style="width:100%">
                 <div class="left floated column">
                     <a @click="goBack" class="item borderless">
                         <i class="large angle left icon grey"></i>
