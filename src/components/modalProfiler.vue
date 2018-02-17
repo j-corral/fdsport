2b73d4b87fba7fda1a074c26e0ee4c904a3a2e2e
Jonathan C <johnlejardinnier@gmail.com>
2018-02-17 16:56:26 +0100

    feat(profiler): improve profiler data

--- src/components/modalProfiler.vue
+++ src/components/modalProfiler.vue
@@ -4,31 +4,62 @@
             <i class="large red bug icon center aligned" style="padding-left: 10px;"></i>
         </button>
         <sui-modal v-model="profiler">
-            <sui-modal-header>Profiling utilisateur</sui-modal-header>
+            <sui-modal-header>Profiling utilisateur et produit</sui-modal-header>
             <sui-modal-content image>
                 <sui-modal-description v-if="this.user" class="scrolling content">
                     <div>
-                        <p class="ui header">Informations</p>
+                        <p class="ui header">Informations utilisateur</p>
                         <p><strong>ID: </strong> {{this.user.user_id}}</p>
                         <p><strong>Prénom: </strong> {{this.user.firstname}}</p>
                         <p><strong v-if="this.user.lastname">Nom: </strong> {{this.user.lastname}}</p>
                     </div>
                     <div v-if="this.user.axe">
                         <div class="ui divider"></div>
-                        <p class="ui header">Axes</p>
+                        <p class="ui header">Axes utilisateur</p>
                         <p><strong>Homme : </strong>{{this.user.axe.male}}</p>
                         <p><strong>Femme : </strong>{{this.user.axe.female}}</p>
                         <p><strong>Age : </strong>{{this.user.axe.age}}</p>
                         <p><strong>CSP : </strong>{{this.user.axe.csp}}</p>
-                        <p><strong>Brand : </strong>{{this.user.axe.brand}}</p>
-                        <p><strong>City : </strong>{{this.user.axe.city}}</p>
+                        <p><strong>Marque : </strong>{{this.user.axe.brand}}</p>
+                        <p><strong>Ville : </strong>{{this.user.axe.city}}</p>
                         <p><strong>Sport : </strong>{{this.user.axe.sport}}</p>
                     </div>
-                    <!--<div>
+                    <div v-if="this.item">
                         <div class="ui divider"></div>
-                        <p class="ui header">Articles consultés</p>
-                        Todo get clicks by user id
-                    </div>-->
+                        <p class="ui header">Informations du dernier produit visité</p>
+                        <p v-if="this.item.product_id"><strong>Type : </strong>Article</p>
+                        <p v-else><strong>Type : </strong>Billet de match</p>
+                        <!--<p v-if="this.item.sport.name"><strong>Type de match : </strong>{{this.item.sport.name}}</p>-->
+                        <p><strong>ID produit : </strong>{{this.item.product_id || this.item.ticket_id}}</p>
+                        <!--<p><strong>Nom produit : </strong>{{this.item.name || this.item.home}}</p>-->
+                        <p><strong>Prix produit : </strong>{{this.item.price}}€</p>
+                        <p v-if="this.item.is_fixed"><strong>Flottant: </strong>{{!this.item.is_fixed}}</p>
+                    </div>
+                    <div v-if="this.item">
+                        <div class="ui divider"></div>
+                        <p class="ui header">Axes du dernier produit visité</p>
+                        <p><strong>Homme: </strong>{{!this.item.axe.male}}</p>
+                        <p><strong>Femme : </strong>{{this.item.axe.female}}</p>
+                        <p><strong>Age : </strong>{{this.item.axe.age}}</p>
+                        <p><strong>CSP : </strong>{{this.item.axe.csp}}</p>
+                        <p><strong>Marque : </strong>{{this.item.axe.brand}}</p>
+                        <p><strong>Ville : </strong>{{this.item.axe.city}}</p>
+                        <!--<p><strong>Sport : </strong>{{this.item.axe.sport.name}}</p>-->
+                    </div>
+                    <div v-if="clicks">
+                        <div class="ui divider"></div>
+                        <p class="ui header">Historique de consultation (antéchronologique)</p>
+                        <div v-for="click in clicks" :key="click.click_id">
+                            <p v-if="click.product">
+                                Article N° {{click.product.product_id}} :
+                                <router-link target="_blank" :to="{ name: 'shop.view', params:{id:click.product.product_id}}">{{click.product.name}}</router-link>
+                            </p>
+                            <p v-if="click.ticket">
+                                Billet N°{{click.ticket.ticket_id}}:
+                                <router-link target="_blank" :to="{ name: 'tickets.view', params:{id:click.ticket.ticket_id}}"> {{click.ticket.home}}/{{click.ticket.visitor}}</router-link>
+                            </p>
+                        </div>
+                    </div>
                 </sui-modal-description>
                 <sui-modal-description v-else>
                     <sui-header>Données récupérées</sui-header>
--- src/js/profiler.js
+++ src/js/profiler.js
@@ -2,7 +2,10 @@ export default {
     data() {
         return {
             user:false,
-            profiler:false
+            profiler:false,
+            item:false,
+            clicks:false,
+            loading:false
         }
     },
     methods: {
@@ -12,9 +15,34 @@ export default {
                 this.user = JSON.parse(usr)
             }
         },
+        getItem() {
+            let item = this.$cookies.get('item')
+            this.item = false
+            if(item) {
+                this.item = JSON.parse(item)
+            }
+        },
+        getClicks(){
+            this.$clicks = this.$resource('clicks/user{/cookie}', {}, {}, {
+                before: () => {
+                    this.loading = true
+                },
+                after: () => this.loading = false
+            })
+            let cookieName = this.$cookies.get('user_id')
+            if(cookieName) {
+                this.$clicks.query({cookie: cookieName}).then((response) => {
+                    this.clicks = response.data
+                }, (response) => {
+                    console.log('error', response)
+                })
+            }
+        },
         toggleDebug() {
             console.log('toggle profiler')
             this.getUser()
+            this.getItem()
+            this.getClicks()
             this.profiler = !this.profiler
         }
     }
